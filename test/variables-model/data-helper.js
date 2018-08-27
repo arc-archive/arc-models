1e414d8e1500514386ccd01d806ac628b53e8d13
Pawel Psztyc <jarrodek@gmail.com>
2018-08-26 22:52:31 -0700

    New: Adding variables model

---
+++ test/variables-model/data-helper.js
@@ -0,0 +1,27 @@
+const DataHelper = {};
+/* global PouchDB */
+DataHelper.addVars = function(items) {
+  const db = new PouchDB('variables');
+  if (items instanceof Array) {
+    return db.bulkDocs(items);
+  }
+  return db.post(items);
+};
+DataHelper.addEnv = function(names) {
+  const db = new PouchDB('variables-environments');
+  if (names instanceof Array) {
+    names = names.map(function(item) {
+      return {
+        name: item
+      };
+    });
+  } else {
+    names = {
+      name: names
+    };
+  }
+  if (names instanceof Array) {
+    return db.bulkDocs(names);
+  }
+  return db.post(names);
+};
