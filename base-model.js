517843186bd47e8c241c9320148257d271035885
Pawel Psztyc <jarrodek@gmail.com>
2019-04-01 18:44:22 -0700

    Update: Updating import paths to match web spec

--- base-model.js
+++ base-model.js
@@ -11,8 +11,8 @@ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 License for the specific language governing permissions and limitations under
 the License.
 */
-import {EventsTargetMixin} from '@advanced-rest-client/events-target-mixin/events-target-mixin.js';
-import '@advanced-rest-client/uuid-generator/uuid-generator.js';
+import {EventsTargetMixin} from '../../@advanced-rest-client/events-target-mixin/events-target-mixin.js';
+import '../../@advanced-rest-client/uuid-generator/uuid-generator.js';
 /**
  * A base class for all models.
  *
