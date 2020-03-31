6fdd77c6c099d41c7925a0816ef879a6486cf0a9
Pawel <jarrodek@gmail.com>
2020-03-30 19:48:30 -0700

    test: adding tests for advanced-rest-client/arc-electron#183

--- test/url-indexer/url-indexer-query.test.js
+++ test/url-indexer/url-indexer-query.test.js
@@ -60,7 +60,7 @@ describe('<url-indexer> - Indexing test', function() {
           ['mulesoft.com', 0, 0, 0]
         ].forEach(function(data) {
           it(`Querying for "${data[0]}"`, function() {
-            return element.query(data[0], {detailed: false})
+            return element.query(data[0], { detailed: false })
             .then((result) => {
               assert.lengthOf(Object.keys(result), data[1]);
             });
@@ -96,7 +96,7 @@ describe('<url-indexer> - Indexing test', function() {
           ['mulesoft.com', 1, 0, 1]
         ].forEach(function(data) {
           it(`Querying for "${data[0]}"`, function() {
-            return element.query(data[0], {detailed: true})
+            return element.query(data[0], { detailed: true })
             .then((result) => {
               assert.lengthOf(Object.keys(result), data[1]);
             });
@@ -122,6 +122,145 @@ describe('<url-indexer> - Indexing test', function() {
             });
           });
         });
+      });
+    });
+
+    describe('IP based search', () => {
+      let element;
+      before(async () => {
+        element = await basicFixture();
+        const toIndex = [{
+          id: 'test-id-1',
+          url: 'https://178.1.2.5/api/test1',
+          type: 'history'
+        }, {
+          id: 'test-id-2',
+          url: 'https://178.1.2.5/api/test2',
+          type: 'history'
+        }, {
+          id: 'test-id-3',
+          url: 'https://178.1.2.5/api/test3',
+          type: 'history'
+        }, {
+          id: 'test-id-4',
+          url: 'https://128.1.2.5/api/test4',
+          type: 'history'
+        }];
+        await element.index(toIndex);
+      });
+
+      after(async () => {
+        const db = await element.openSearchStore();
+        await clearAllIndexes(db);
+      });
+
+      const groupMatch = {
+        'test-id-1': 'history',
+        'test-id-2': 'history',
+        'test-id-3': 'history',
+      };
+
+      const pathMatch = {
+        'test-id-1': 'history',
+        'test-id-2': 'history',
+        'test-id-3': 'history',
+        'test-id-4': 'history',
+      };
+
+      it('returns data for a partial IP address (1st group)', async () => {
+        const result = await element.query('178', {
+          detailed: false,
+          type: 'history'
+        });
+        assert.deepEqual(result, groupMatch);
+      });
+
+      it('returns data for a partial IP address (2nd group)', async () => {
+        const result = await element.query('178.1', {
+          detailed: false,
+          type: 'history'
+        });
+        assert.deepEqual(result, groupMatch);
+      });
+
+      it('returns data for a partial IP address (3rd group)', async () => {
+        const result = await element.query('178.1.2', {
+          detailed: false,
+          type: 'history'
+        });
+        assert.deepEqual(result, groupMatch);
+      });
+
+      it('returns data for a partial IP address (4th group)', async () => {
+        const result = await element.query('178.1.2.5', {
+          detailed: false,
+          type: 'history'
+        });
+        assert.deepEqual(result, groupMatch);
+      });
+
+      it('matches path', async () => {
+        const result = await element.query('/api', {
+          detailed: false,
+          type: 'history'
+        });
+        assert.deepEqual(result, pathMatch);
+      });
+
+      it('matches path without first slash', async () => {
+        const result = await element.query('api', {
+          detailed: false,
+          type: 'history'
+        });
+        assert.deepEqual(result, pathMatch);
+      });
+
+      it('matches path (detailed)', async () => {
+        const result = await element.query('/api', {
+          detailed: true,
+          type: 'history'
+        });
+        assert.deepEqual(result, pathMatch);
+      });
+
+      it('matches path without first slash (detailed)', async () => {
+        const result = await element.query('api', {
+          detailed: true,
+          type: 'history'
+        });
+        assert.deepEqual(result, pathMatch);
+      });
+
+      it('matches specific path', async () => {
+        const result = await element.query('/api/test4', {
+          detailed: false,
+          type: 'history'
+        });
+        assert.equal(result['test-id-4'], 'history');
+      });
+
+      it('matches specific path without first slash', async () => {
+        const result = await element.query('api/test4', {
+          detailed: false,
+          type: 'history'
+        });
+        assert.equal(result['test-id-4'], 'history');
+      });
+
+      it('matches specific path (detailed)', async () => {
+        const result = await element.query('/api/test4', {
+          detailed: true,
+          type: 'history'
+        });
+        assert.equal(result['test-id-4'], 'history');
+      });
+
+      it('matches specific path without first slash (detailed)', async () => {
+        const result = await element.query('api/test4', {
+          detailed: true,
+          type: 'history'
+        });
+        assert.equal(result['test-id-4'], 'history');
       });
     });
   });
