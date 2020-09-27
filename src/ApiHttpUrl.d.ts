4c4183c463af58e4f2292234ac4ccb2f192cf24f
Pawel <jarrodek@gmail.com>
2020-09-26 18:08:32 -0700

    feat: adding base definition for future API projects

---
+++ src/ApiHttpUrl.d.ts
@@ -0,0 +1,35 @@
+import { ApiType } from './ApiTypes';
+
+export declare class ApiHttpUrl {
+  /**
+   * The value of the URL.
+   */
+  path?: string;
+  /**
+   * The host value of the URL
+   */
+  host?: string;
+  /**
+   * The protocol value of the URL
+   */
+  protocol?: string;
+  /**
+   * The query parameters.
+   */
+  query?: ApiType[];
+
+  /**
+   * @param {ApiHttpUrl=} input Either ApiHttpUrl object from the data store or a string to parse
+   */
+  constructor(input?: ApiHttpUrl);
+
+  /**
+   * @returns An URL value from current properties.
+   */
+  toString(): string;
+
+  /**
+   * Creates a new instance of the class from a string value.
+   */
+  static fromValue(input: string): ApiHttpUrl;
+}
---
+++ src/ApiHttpUrl.js
@@ -0,0 +1,69 @@
+import { UrlParser } from '@advanced-rest-client/url-parser';
+
+/** @typedef {import('./ApiTypes').ApiType} ApiType */
+/** @typedef {import('./ApiHttpUrl').ApiHttpUrl} ApiHttpUrlInput */
+
+export class ApiHttpUrl {
+  /**
+   * @param {ApiHttpUrlInput=} input Either ApiHttpUrl object from the data store or a string to parse
+   */
+  constructor(input={}) {
+    /**
+     * The value of the URL.
+     * @type {string|undefined}
+     */
+    this.path = input.path;
+    /**
+     * The host value of the URL
+     * @type {string|undefined}
+     */
+    this.host = input.host;
+    /**
+     * The protocol value of the URL
+     * @type {string|undefined}
+     */
+    this.protocol= input.protocol;
+    /**
+     * The query parameters.
+     * @type {ApiType[]|undefined}
+     */
+    this.query = input.query;
+  }
+
+  /**
+   * @returns {string} An URL value from current properties.
+   */
+  toString() {
+    const parser = new UrlParser('');
+    parser.protocol = this.protocol;
+    parser.host = this.host;
+    parser.path = this.path;
+    parser.searchParams = (this.query || []).map((item) => { return [ item.name, item.value ]; });
+    return parser.toString();
+  }
+
+  /**
+   * Creates a new instance of the class from a string value.
+   * @static
+   * @param {string} input
+   * @returns {ApiHttpUrl}
+   */
+  static fromValue(input) {
+    const parser = new UrlParser(input);
+    const query = (parser.searchParams || []).map((item) => {
+      const [name, value] = item;
+      return /** @type ApiType */ ({
+        name,
+        value,
+        type: 'string',
+        enabled: true,
+      });
+    });
+    return new ApiHttpUrl({
+      protocol: parser.protocol,
+      host: parser.host,
+      path: parser.path,
+      query,
+    });
+  }
+}
