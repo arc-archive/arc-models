1ab1db1d4e72da7e35eaa5732ff77e0bc9770c5f
Pawel Psztyc <jarrodek@gmail.com>
2021-03-15 12:57:15 -0700

    refactor: moving HAR transformer to its own repo
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

--- index.js
+++ index.js
@@ -140,4 +140,3 @@ export { ImportNormalize } from './src/lib/ImportNormalize.js';
 export { ImportFactory } from './src/lib/ImportFactory.js';
 export { isSingleRequest, isPostman, isArcFile, prepareImportObject, readFile, isOldImport, isObject } from './src/lib/ImportUtils.js';
 export * as DataSize from './src/lib/DataSize.js';
-export { HarTransformer } from './src/transformers/HarTransformer.js';
